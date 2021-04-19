2454077f5193dd6706b98211f0f8677f5c5cb6ef
Pgsalbuquerque <albuquerquepgs@gmail.com>
2021-04-19 17:33:44 -0300

    tela dashboard

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Main, Titulo, Grid, Card, TituloCard, Image } from './styles';
+
+function dashboard() {
+  return (
+      <Main>
+        <Titulo>Dashboard</Titulo>
+        <Grid>
+            <Card href="/recompensainserir" >
+                <TituloCard>Inserir recompensa</TituloCard>
+                <Image src="recompensainserir.svg"></Image>
+            </Card>
+            <Card href="/cadastro" >
+                <TituloCard>Cadastrar usuário </TituloCard>
+                <Image src="cadastrarusuario.svg"></Image>
+            </Card>
+            <Card href="/medidasinserir" >
+                <TituloCard>Atualizar medidas</TituloCard>
+                <Image src="atualizarmedidas.svg"></Image>
+            </Card>
+            <Card href="/publicarfeed" >
+                <TituloCard >Postar no feed</TituloCard>
+                <Image src="postarfeed.svg"></Image>
+            </Card>
+        </Grid>
+      </Main>
+  )
+}
+
+export default dashboard;
---
+++ src/pages/dashboard/styles.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+
+export const Main = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: start;
+
+`;
+
+export const Titulo = styled.p`
+    font-size: 50px;
+    color: #fff;
+    font-family: "Fredoka One",Arial, Helvetica, sans-serif;
+    margin-top: 100px;
+`;
+
+export const Grid = styled.div`
+    margin-top: 40px;
+    display: grid;
+    grid-template-columns: 250px 250px;
+    grid-template-rows: 150px 150px;
+    grid-column-gap: 50px;
+    grid-row-gap: 50px;
+`;
+
+export const Card = styled.a`
+    width: 250px;
+    height: 150px;
+    display: flex;
+    flex-direction: column;
+    border: none;
+    outline: none;
+    cursor: pointer;
+`;
+
+export const TituloCard = styled.p`
+    font-size: 16px;
+    color: #fff;
+    height: 20%;
+    width: 100%;
+    text-align: center;
+    font-family: "Fredoka One",Arial, Helvetica, sans-serif;
+`;
+
+export const Image = styled.img`
+    height: 80%;
+    width: 100%;
+`;
