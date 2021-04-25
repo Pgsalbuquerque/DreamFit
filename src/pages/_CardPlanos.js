618144e30482e44b6a35a98d6caa3073ad9887de
Pgsalbuquerque <albuquerquepgs@gmail.com>
2021-04-25 17:10:20 -0300

    fix bugs

--- src/pages/_CardPlanos.js
+++ src/pages/_CardPlanos.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
 import { Main, Yellow, White, Telinha, Plano, PlanoNome, TextApartir, Valor, DescPlanos,
-Linha, Diferencial, Items, Button } from '../styles/cardplanostyles';
+Linha, Diferencial, Items, Button, DivBlack, DivInvisiblePlano } from '../styles/cardplanostyles';
 
 export default function pages(props) {
   return (
       <Main>
-          <Telinha>
-              <TextApartir>A partir de</TextApartir>
-              <Valor>{props.valor}</Valor>
-          </Telinha>
           <Yellow>
-            <Plano>Plano</Plano>
-            <PlanoNome>{props.nome}</PlanoNome>
+            <DivInvisiblePlano>
+              <Plano>Plano</Plano>
+              <PlanoNome>{props.nome}</PlanoNome>
+            </DivInvisiblePlano>
+            <DivBlack></DivBlack>
           </Yellow>
           <White>
+            <Telinha>
+                <TextApartir>A partir de</TextApartir>
+                <Valor>{props.valor}</Valor>
+            </Telinha>
             <DescPlanos>Ganhe pontos a cada dia treinado e troque por recompensas</DescPlanos>
             <Linha></Linha>
             <Diferencial>Aulas</Diferencial>
--- src/styles/cardplanostyles.js
+++ src/styles/cardplanostyles.js
@@ -30,15 +30,27 @@ export const White = styled.div`
 `;
 
 export const Telinha = styled.div`
-    height: 83px;
+    height: 100px;
     width: 250px;
-    position: absolute;
     background: #000;
-    margin-top: 6%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+`;
+
+export const DivInvisiblePlano = styled.div`
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const DivBlack = styled.div`
+    height: 20%;
+    width: 250px;
+    background: #000;
 `;
 
 export const Plano = styled.p`
@@ -64,12 +76,13 @@ export const Valor = styled.p`
     font-size: 42px;
     color: #7B2CBF;
     font-family: "Fredoka One",Arial, Helvetica, sans-serif;
+    margin-bottom: 15px;
 `
 
 export const DescPlanos = styled.p`
     font-size: 22px;
     color: #000;
-    margin-top: 30%;
+    margin-top: 20px;
     text-align: center;
     width: 95%;
 `;
